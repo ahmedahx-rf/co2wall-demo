87bcfddd29ccdb32e1b193d3c2dfc6cf8dc95aba
Ahmed Ahmed <ahmed@rf-frontend.de>
2025-03-07 13:55:04 +0100

    restructure

--- co2wall/src/app.routes.ts
+++ co2wall/src/app.routes.ts
@@ -5,6 +5,7 @@ import { MonitoringPage } from './app/pages/monitoring/monitoring';
 export const appRoutes: Routes = [
     {
         path: '',
+        title: 'CO2Wall',
         component: AppLayout,
         children: [{ path: '', title: 'CO2Wall', component: MonitoringPage }]
     },
